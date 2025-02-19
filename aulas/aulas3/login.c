3a84fc0e1503dab4fba7a83d5c87c6a56f0fb135
PedroAntonioAO <pototo2906@gmail.com>
2025-02-19 18:02:06 +0000

    add login.c

---
+++ aulas/aulas3/login.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+int main(){
+
+  int Cpf,senha;
+
+  printf("Digite seu Cpf: ");
+  scanf("%d",&Cpf);
+
+  printf("Digite sua senha: ");
+  scanf("%d",&senha);
+
+  printf("Seu Cpf e: %d\n",Cpf);
+  printf("Sua senha e: %d\n",senha);
+
+  return 0;
+}
