7fb40a47d8a88f5aa0ab06bf836f78a4b761ebf0
PedroAntonioAO <pototo2906@gmail.com>
2025-03-12 17:51:14 +0000

    aula5

---
+++ aulas/aula5/exemplofor.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+
+int main() {
+  int i;
+
+  for (i=0; i<15; i++) {
+    printf( "%d - We are born of the blood, made men by the blood, undone by the blood. Our eyes are yet to open. Fear the Old Blood.\n",i );
+ }
+  
+  return 0;
+}
